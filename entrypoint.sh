c403d7f4976a850dafbfd7dafff6713b4579e18c
Sergiy Mshtaler <svmashtaler@gmail.com>
2025-12-19 14:29:42 +0100

    fix: stdout

--- entrypoint.sh
+++ entrypoint.sh
@@ -102,7 +102,7 @@ api() {
     echo >&2 "path: $path"
     echo >&2 "response: $response"
     if [[ "$response" == *'"Server Error"'* ]]; then
-      echo "Server error - trying again"
+      echo >&2 "Server error - trying again"
     else
       exit 1
     fi
@@ -149,7 +149,7 @@ trigger_workflow() {
   echo >&2 "  {\"ref\":\"${ref}\",\"inputs\":${client_payload}}"
 
   api "workflows/${INPUT_WORKFLOW_FILE_NAME}/dispatches" \
-    --data "{\"ref\":\"${ref}\",\"inputs\":${client_payload}}"
+    --data "{\"ref\":\"${ref}\",\"inputs\":${client_payload}}" > /dev/null
 
   NEW_RUNS=$OLD_RUNS
   while [ "$NEW_RUNS" = "$OLD_RUNS" ]
